99042a698bbb9d99140cbc67a3ddb8175a3b663f
Viral Prajapati (a.k.a KAAL) <viral.prajapati.nmims@gmail.com>
2025-09-05 14:03:59 +0530

    Update WorkExperienceData.ts

--- src/pages/raw-data/WorkExperienceData.ts
+++ src/pages/raw-data/WorkExperienceData.ts
@@ -1,13 +1,13 @@
 
 const WorkExperienceData ={
-    jobTitle:['Software Developer Trainee (Java + React Stack)', 'Technical Support Engineer','L2 Technical Support Engineer','Technical Support Executive','Customer Success Engineer','Application Support Technician'
+    jobTitle:['Software Developer Trainee (Java + React Stack)', 'Software Engineer','Software Engineer','Software Engineer','Software Engineer','Software Technician'
 ],
-    WorkHeading:['Self-Driven Projects & Freelance Work | 2024 – Present','Quest2Travel, Mumbai | Sep 2024 –  2025','eClinicalWorks, Mumbai | Jan 2022 – Mar 2024','Restolabs, Noida | Oct 2020 – Dec 2021','Contentstack Pvt. Ltd., Virar | Feb 2020 – Aug 2020','eZee Technosys Pvt. Ltd., Surat | May 2019 – Dec 2019'],
+    WorkHeading:['Self-Driven Projects & Freelance Work | 2024 – Present','Quest2Travel, Mumbai | Sep 2024 –  2025','eClinicalWorks, Mumbai | Jan 2022 – Mar 2024','Contentstack Pvt. Ltd., Virar | Feb 2020 – Aug 2020','eZee Technosys Pvt. Ltd., Surat | May 2019 – Dec 2019'],
     Responsibilities:['Developed full-stack applications using Java, Spring Boot, and React.js with RESTful APIs.Built an Attendance Management System with admin login, CRUD operations, and real-time reporting using MySQL.Created a responsive E-Book Sharing Platform using HTML, CSS, and JavaScript, enabling secure file upload/download and UI interactivity.Focused on clean code, reusable components, and performance optimization during frontend development.',
-                      'Provided platform-level support for an expense management system using Oracle SQL Developer.Executed advanced DML/DDL queries to troubleshoot backend logic and data integrity issues.Actively collaborated with developers to streamline issue resolution and improve SQL efficiency.',
-                      'Supported US-based clients with critical issues in healthcare applications, ensuring compliance with FHIR and HL7 protocols.Performed in-depth log reviews and SQL debugging (MSSQL/MySQL), enabling rapid resolution of high-priority tickets.Assisted in the implementation of REST APIs for EHR integrations and vendor-side diagnostics.',
-                      'Managed bug/feature documentation and collaborated with development teams for timely updates.Handled client escalations and issue replication for food-tech applications in the US and UK regions.',
-                      'Provided technical onboarding and incident response for Headless CMS clients via REST API monitoring.Used Jira for bug tracking, sprint follow-ups, and real-time client communication.',
+                      'Provided platform-level Software Development Support for an expense management system using Oracle SQL Developer.Executed advanced DML/DDL queries to troubleshoot backend logic and data integrity issues. Actively collaborated with developers to streamline issue resolution and improve SQL efficiency.',
+                      'Software Development Supported US-based clients with critical issues in healthcare applications, ensuring compliance with FHIR and HL7 protocols. Performed in-depth log reviews and SQL debugging (MSSQL/MySQL), enabling rapid resolution of high-priority tickets.Assisted in the implementation of REST APIs for EHR integrations and vendor-side diagnostics.',
+                      'Managed bug/feature documentation and collaborated with development teams for timely updates. Handled client escalations and issue replication for food-tech applications in the US and UK regions.',
+                      'Provided Software Development Support onboarding and incident response for Headless CMS clients via REST API monitoring.Used Jira for bug tracking, sprint follow-ups, and real-time client communication.',
                       'Delivered L1 troubleshooting for international clients, resolving server-side issues and database errors.Monitored software logs and improved the first-response time for issue acknowledgment and resolution.'
                     ]
 
@@ -30,26 +30,26 @@ export default WorkExperienceData;
 //Focused on clean code, reusable components, and performance optimization during frontend development.
 
 
-//Technical Support Engineer
+//Technical Software Development Support Engineer
 
 //Quest2Travel, Mumbai | Sep 2024 – May 2025
 
-//Provided platform-level support for an expense management system using Oracle SQL Developer.
+//Provided platform-level Software Development Support for an expense management system using Oracle SQL Developer.
 
 //Executed advanced DML/DDL queries to troubleshoot backend logic and data integrity issues.
 
 //Actively collaborated with developers to streamline issue resolution and improve SQL efficiency.
 
-//L2 Technical Support Engineer
+//L2 Technical Software Development Support Engineer
 //eClinicalWorks, Mumbai | Jan 2022 – Mar 2024
 
-//Supported U.S.-based clients with critical issues in healthcare applications, ensuring compliance with FHIR and HL7 protocols.
+//Software Development Supported U.S.-based clients with critical issues in healthcare applications, ensuring compliance with FHIR and HL7 protocols.
 
 //Performed in-depth log reviews and SQL debugging (MSSQL/MySQL), enabling rapid resolution of high-priority tickets.
 
 //Assisted in the implementation of REST APIs for EHR integrations and vendor-side diagnostics.
 
-//Technical Support Executive
+//Technical Software Development Support Executive
 //Restolabs, Noida | Oct 2020 – Dec 2021
 
 //Managed bug/feature documentation and collaborated with development teams for timely updates.
@@ -63,7 +63,7 @@ export default WorkExperienceData;
 
 //Used Jira for bug tracking, sprint follow-ups, and real-time client communication.
 
-//Application Support Technician
+//Application Software Development Support Technician
 //eZee Technosys Pvt. Ltd., Surat | May 2019 – Dec 2019
 
 //Delivered L1 troubleshooting for international clients, resolving server-side issues and database errors.
